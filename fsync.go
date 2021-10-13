756d6c02694eb5cf84bece21a50a045e2c8832fb
Tsyren <nsu1team@gmail.com>
2021-10-13 20:24:36 +0800

    support ListenTarget func, detects new files

--- fsync.go
+++ fsync.go
@@ -1,8 +1,10 @@
 package fsync
 
 import (
+	"context"
 	"io/fs"
 	"path/filepath"
+	"time"
 )
 
 /*
@@ -10,7 +12,7 @@ import (
 	1. parse target directory
 	2. save loaded structure into metadata config
 	3. listen a target directory for any changes
-	4. update metadata config if new changes appeared
+	4. update metadata config if new changes have appeared
 
 	Entities:
 	- Metadata config is in-memory hash-map, keys are paths, values are dir structures
@@ -18,26 +20,68 @@ import (
 	- Listener is a blocking process
 */
 
-// Metadata is keys list of files
+// Metadata is keys list of files.
 type Metadata struct {
-	files []string
+	files  []string
+	target string
 }
 
+// Add appends path to files list.
 func (m *Metadata) Add(path string) {
 	m.files = append(m.files, path)
 }
 
-// LoadTargetDir parses Dir and returns list of all paths inside of Dir
+// Includes scans for presence of path in files list.
+func (m *Metadata) Included(path string) bool {
+	for _, file := range m.files {
+		if file == path {
+			return true
+		}
+	}
+
+	return false
+}
+
+// LoadTargetDir parses Dir and returns list of all paths inside of Dir.
 func LoadTargetDir(target string) (*Metadata, error) {
-	metadata := Metadata{}
+	metadata := Metadata{target: target}
 	fn := func(path string, d fs.DirEntry, err error) error {
 		metadata.Add(path)
 
 		return nil
 	}
-	if err := filepath.WalkDir(target, fn); err != nil {
+	if err := filepath.WalkDir(metadata.target, fn); err != nil {
 		return nil, err
 	}
 
 	return &metadata, nil
+}
+
+// ListenTarget checks every N seconds the target directory
+func ListenTarget(ctx context.Context, metadata *Metadata) error {
+	checker := time.NewTicker(1000 * time.Millisecond)
+
+	// checkFn looks for path in metadata
+	// if path is not present, then add to Metadata
+	checkFn := func(path string, d fs.DirEntry, err error) error {
+		if !metadata.Included(path) {
+			metadata.Add(path)
+		}
+
+		return nil
+	}
+
+	for {
+		select {
+		case <-checker.C:
+			// it should handle errors & propogate it to upstream
+			if err := filepath.WalkDir(metadata.target, checkFn); err != nil {
+				return err
+			}
+
+			// TODO: after walking is done, time to delete all old directries and files
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }
